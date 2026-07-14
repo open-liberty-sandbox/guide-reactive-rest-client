d11749ec27544ab5d5f1ca1fcb83d94b140e5839
Manu Paul Kunnumpurathu <kun.manu93@gmail.com>
2026-07-14 11:57:09 +0200

    feat: update the web client
    - add reactive dependencies
    - update the web client to use an alternative reactive provider

--- start/query/src/main/java/io/openliberty/guides/query/client/InventoryClient.java
+++ start/query/src/main/java/io/openliberty/guides/query/client/InventoryClient.java
@@ -1,16 +1,20 @@
 package io.openliberty.guides.query.client;
 
+import java.util.List;
+import java.util.Properties;
+
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.CompletionStage;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.glassfish.jersey.client.rx.rxjava.RxObservableInvoker;
+import org.glassfish.jersey.client.rx.rxjava.RxObservableInvokerProvider;
+
+import rx.Observable;
 
 @RequestScoped
 public class InventoryClient {
@@ -19,7 +23,6 @@ public class InventoryClient {
     @ConfigProperty(name = "INVENTORY_BASE_URI", defaultValue = "http://localhost:9085")
     private String baseUri;
 
-
     public List<String> getSystems() {
         return ClientBuilder.newClient()
                 .target(baseUri)
@@ -27,19 +30,19 @@ public class InventoryClient {
                 .request()
                 .header(HttpHeaders.CONTENT_TYPE,
                         MediaType.APPLICATION_JSON)
-                .get(new GenericType<List<String>>() {
-                });
+                .get(new GenericType<List<String>>() { });
     }
 
-    public CompletionStage<Properties> getSystem(String hostname) {
+    public Observable<Properties> getSystem(String hostname) {
         return ClientBuilder.newClient()
                 .target(baseUri)
+                .register(RxObservableInvokerProvider.class)
                 .path("/inventory/systems")
                 .path(hostname)
                 .request()
                 .header(HttpHeaders.CONTENT_TYPE,
                         MediaType.APPLICATION_JSON)
-                .rx()
-                .get(Properties.class);
+                .rx(RxObservableInvoker.class)
+                .get(new GenericType<Properties>() { });
     }
 }
